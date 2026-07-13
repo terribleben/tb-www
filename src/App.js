1b91b8d986454cee94563fad787573f8bcdba870
Ben <spongebucket@gmail.com>
2026-07-13 16:39:48 -0700

    reorg commented items into hidden section

--- src/App.js
+++ src/App.js
@@ -46,11 +46,6 @@ const sections = [
         url: "https://www.youtube.com/watch?v=3jPsO2jIU20",
         description: "a visualizer created with Hydra"
       },
-      /* {
-        name: "Baikal",
-        url: "https://www.youtube.com/watch?v=HjcY6ooLjvM",
-        description: "a visualizer created with Hydra"
-      }, */
       {
         name: "norns-sandbox",
         url: "https://github.com/bitgraves/norns-sandbox",
@@ -71,32 +66,12 @@ const sections = [
         url: "https://castle.xyz/d/nWSp1fQdX",
         description: "another dreamcore game"
       },
-      /* {
-        name: "Untitled Dungeon",
-        url: "https://terribleben.itch.io/untitled-dungeon",
-        description: "a small pixel world which hides a mystery"
-      }, */
-      /* {
-        name: "Untitled Dungeon OST",
-        url: "https://alcala-roth.bandcamp.com/album/untitled-dungeon-ost",
-        description: "the soundtrack for that game"
-      }, */
-      /* {
-        name: "Expo",
-        url: "http://expo.dev",
-        description: "an app creation tool"
-      }, */
       {
         name: "PitShift",
         url:
           "https://reading.supply/@ben/implementing-a-pitch-shifter-in-supercollider-Z0fcAX",
         description: "a SuperCollider pitch shifter"
       },
-      /* {
-        name: "Circloid",
-        url: "https://github.com/terribleben/circloid",
-        description: "a lua game"
-      }, */
       {
         name: "Structures",
         url: "https://alcala-roth.bandcamp.com/album/structures",
@@ -107,31 +82,6 @@ const sections = [
         url: "https://alcala-roth.bandcamp.com/album/am-sonora",
         description: "synthesizer border music"
       },
-      /* {
-        name: "chuck-mode",
-        url: "https://github.com/terribleben/chuck-mode",
-        description: "emacs and ChucK"
-      },
-      {
-        name: "Endless Library",
-        url: "https://github.com/terribleben/endless-library",
-        description: "a procedurally generated library"
-      },
-      {
-        name: "Goat machine",
-        url: "/demo/goatmachine0",
-        description: "a WebAudio formant synthesizer"
-      }, */
-      /* {
-        name: "Terrible Sync",
-        url: "https://github.com/terribleben/terrible-sync",
-        description: "a voltage metronome"
-      }, */
-      /* {
-        name: "Runabout",
-        url: "https://github.com/terribleben/runabout",
-        description: "an abstract lander game"
-      }, */
       {
         name: "Torus",
         url: "https://www.youtube.com/watch?v=vNhct2wUPIc",
@@ -157,6 +107,63 @@ const sections = [
         name: "Github",
         url: "https://github.com/terribleben",
         description: "source code"
+      }
+    ]
+  },
+  {
+    id: "hidden",
+    title: "Hidden",
+    hidden: true,
+    items: [
+      {
+        name: "Baikal",
+        url: "https://www.youtube.com/watch?v=HjcY6ooLjvM",
+        description: "a visualizer created with Hydra"
+      },
+      {
+        name: "Untitled Dungeon",
+        url: "https://terribleben.itch.io/untitled-dungeon",
+        description: "a small pixel world which hides a mystery"
+      },
+      {
+        name: "Untitled Dungeon OST",
+        url: "https://alcala-roth.bandcamp.com/album/untitled-dungeon-ost",
+        description: "the soundtrack for that game"
+      },
+      {
+        name: "Expo",
+        url: "http://expo.dev",
+        description: "an app creation tool"
+      },
+      {
+        name: "Circloid",
+        url: "https://github.com/terribleben/circloid",
+        description: "a lua game"
+      },
+      {
+        name: "chuck-mode",
+        url: "https://github.com/terribleben/chuck-mode",
+        description: "emacs and ChucK"
+      },
+      {
+        name: "Endless Library",
+        url: "https://github.com/terribleben/endless-library",
+        description: "a procedurally generated library"
+      },
+      {
+        name: "Goat machine",
+        url: "/demo/goatmachine0",
+        description: "a WebAudio formant synthesizer"
+      },
+      {
+        name: "Terrible Sync",
+        url: "https://github.com/terribleben/terrible-sync",
+        description: "a voltage metronome"
+      },
+      {
+        name: "Runabout",
+        url: "https://github.com/terribleben/runabout",
+        description: "an abstract lander game"
       }
     ]
   }
@@ -190,7 +197,7 @@ export default class App extends Component {
     return (
       <div id="container">
         <div id="content">
-          {sections.map(this._renderSection)}
+          {sections.filter(section => !section.hidden).map(this._renderSection)}
           <div id="intro">
             <p>
               <span id="ben">Ben</span>, a programmer and musician living in
